4de5537c8e2a22445b8731c5245bb98f31ed434a
Diogo Nunes <up201808546@fe.up.pt>
2021-04-01 18:20:24 +0100

    GA working, but a problem in setGridContent

--- TP1/src/geneticAlgorithm.py
+++ TP1/src/geneticAlgorithm.py
@@ -5,11 +5,35 @@ from utils import *
 
 
 def crossover(sol1, sol2):
-    return 0
+    r1, r2 = routersPlaced(sol1), routersPlaced(sol2)
+    minRouters = min(r1, r2)
+    rand = random.randint(1, max(1, minRouters - 1))
+
+    child = []
+
+    for i in range(len(sol1)):
+        if i < rand:
+            child.append(sol1[i])
+        else:
+            child.append(sol2[i])
+
+    return child
 
 
 def mutation(sol1, sol2):
-    return 0
+    r1, r2 = routersPlaced(sol1), routersPlaced(sol2)
+    minRouters = min(r1, r2)
+    rand = random.randint(1, max(1, minRouters - 1))
+
+    mutated = []
+
+    for i in range(len(sol1)):
+        if i != rand:
+            mutated.append(sol1[i])
+        else:
+            mutated.append(sol2[i])
+
+    return sol1
 
 
 def generateInitialPopulation(blueprint):
@@ -43,17 +67,35 @@ def generateInitialPopulation(blueprint):
 
     population.sort(reverse=True, key=lambda elem: value(blueprint, elem))
 
-    for i in population:
-        print(i)
-        print(value(blueprint, i))
-        print("")
-
     return population
 
 
 def geneticAlgorithm(blueprint):
     population = generateInitialPopulation(blueprint)  # 1
+    iteration = 0
 
+    while True:
+        nextGeneration = []
+
+        for i in range(len(population)):
+            x = random.randint(0, int(len(population) / 2))
+            y = random.randint(0, int(len(population) / 2))
+            child = crossover(population[x], population[y])
+
+            if random.randint(0, 100) < 50:  # to change to 10, 5, ... ?
+                child = mutation(population[x], population[y])
+
+            child.sort(reverse=True, key=lambda x: x)
+            if value(blueprint, child) is not None:
+                nextGeneration.append(child)
+
+        nextGeneration.sort(reverse=True, key=lambda elem: value(blueprint, elem))
+        population = nextGeneration
+        iteration += 1
+        if iteration == 20:
+            break
+
+    return max(population, key=lambda elem: value(blueprint, elem))
 
     """
     1. Generate initial population: 30 (of solutions: lists of routers): DONE
@@ -77,7 +119,14 @@ if __name__ == "__main__":
 
     blueprint.printGrid()
 
-    geneticAlgorithm(blueprint)
+    a = geneticAlgorithm(blueprint)
+    print("Genetic Algorithm")
+    print(str(value(blueprint, a)) + " points")
+    print("Router solution: " + str(a))
+
+    # for sol in a:
+    #     setGridContent(blueprint.grid, "r", sol)
+    blueprint.printGrid()
 
     endTime = time.process_time()
     print(f"Time: {endTime - startTime} seconds")
--- TP1/src/utils.py
+++ TP1/src/utils.py
@@ -13,9 +13,9 @@ def distance(pointA, pointB):
 def setGridContent(grid, content, x, y=None):
     try:
         if type(x) == tuple:
-            grid[x[0]][x[1]] = content
+            grid[x] = content
             return
-        grid[x][y] = content
+        grid[(x, y)] = content
         return
     except IndexError:
         return None
