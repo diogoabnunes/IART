2cfa33c9fa2c18782f186cfeb0c0f8e76ee2df02
afonsocaiado <afonsocaiado1@gmail.com>
2021-04-02 17:36:41 +0100

    Main now calls correct functions

--- TP1/src/main.py
+++ TP1/src/main.py
@@ -1,25 +1,20 @@
-from geneticAlgorithm import *
-from hillClimbing import *
-from tabuSearch import *
+import geneticAlgorithm
+import hillClimbing
+import tabuSearch
+import utils
+import blueprint as bp
+import time
 
 
 def simulatedAnnealing():
     print("Algorithm: Simulated Annealing (not implemented yet)\n")
 
 
-def hillClimbing():
-    print("Algorithm: Hill Climbing (not implemented yet)\n")
-
-
-def tabuSearch():
-    print("Algorithm: Tabu Search (not implemented yet)\n")
-
-
 def menu():
-    print("IART - Router Placement\n")
+    print("IART - Router Placement")
 
     while True:
-        print("File input")
+        print("\nFile input")
         print("[1] example.in")
         print("[2] charleston_road.in")
         print("[3] rue_de_londres.in")
@@ -35,7 +30,7 @@ def menu():
         elif file == str(3):
             file = "../inputs/rue_de_londres.in"
         elif file == str(4):
-            file = "../inputs/rue_de_londres.in"
+            file = "../inputs/opera.in"
         elif file == str(5):
             file = "../inputs/lets_go_higher.in"
         elif file == str(0):
@@ -44,35 +39,69 @@ def menu():
             print("File not found\n")
             continue
         print("File input: " + file + "\n")
+        
+        blueprint = bp.Blueprint(file)
+        
+        blueprint.printGrid()
 
-        print("Choose algorithm to run")
+        print("\nChoose algorithm to run")
         print("[1] Simulated Annealing")
         print("[2] Hill Climbing")
         print("[3] Genetic Algorithm")
         print("[4] Tabu Search")
         print("[0] Quit")
         val = input("Option: ")
-
-        blueprint = bp.Blueprint(file)
+    
 
         startTime = time.time()
+        
+        while True:
+            solution = utils.generateMaxRoutersSolution(blueprint)
+            if not utils.validSolution(blueprint, solution):
+                continue
+            if utils.value(blueprint, solution) is None:
+                continue
+            break
 
         if val == str(1):
             simulatedAnnealing()
         elif val == str(2):
-            hillClimbing()
+            hillClimbing.hillClimbing(blueprint, solution)
         elif val == str(3):
-            geneticAlgorithm(blueprint)
+            solution = geneticAlgorithm.geneticAlgorithm(blueprint)
         elif val == str(4):
-            tabuSearch()
-        elif val == str(0):
+            tabuSearch.TabuSearch(blueprint, solution)
+        elif val == str(0): 
             break
         else:
             print("Algorithm not found\n")
             continue
 
         endTime = time.time()
-        print(f"Time: {endTime - startTime} seconds\n\n")
+        print(f"\nTime: {endTime - startTime} seconds\n\n")
+        
+        for sol in solution:
+            bp.setGridContent(blueprint.grid, "r", sol)
+        blueprint.printGrid()
+        
+        print("\nWhat do you wish to do?")
+        print("[1] Start Over")
+        print("[2] Quit")
+        option = input("Option: ")
+        
+        if option == str(1):
+            continue
+        elif option == str(2):
+            print("\nAre you sure you want to leave?")
+            print("[1] No, I am staying!")
+            print("[2] Yes...")
+            option1 = input("Option: ")
+            
+            if option1 == str(1):
+                continue
+            elif option1 == str(2):
+                break
+                
 
 
 if __name__ == "__main__":
